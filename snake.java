0910e87634bb5ff4986b9d9e24a084e24016f7d7
gitbash <kolhenagesh2017@gmail.com>
2021-05-15 00:11:19 +0530

    Added : Option

--- snake.java
+++ snake.java
@@ -1,12 +1,30 @@
 import static java.lang.Math.random;
 public class snake {
+      public static final int NO_PLAY = 0;
+      public static final int LADDER = 1;
+      public static final int SNAKE = 2;
 
+ public static void main(String[] args) {
+      int Start = 0;
+      int Roll = 0;
 
-  public static void main(String[] args) {
-
-   int Value =(int) Math.floor((Math.random() * 6) + 1 );
+      int Value =(int) Math.floor((Math.random() * 6) + 1 );
       System.out.println("Die Number Value: " + Value);
 
+      int option =(int) Math.floor((Math.random() * 10) % 3 );
+      System.out.println("Option: " + option);
 
+      switch (option) {
+             case NO_PLAY:
+                   Roll = Roll;
+                   break;
+             case LADDER:
+                   Roll = + Value;
+                   break;
+             case SNAKE:
+                   Roll = -Value;
+     }
+     int Position =(Start + Roll);
+     System.out.println("Current Position: " + Position);
   }
 }
